1e83345a6189012c4881a3fadc492fee456d23fb
olojo-99 <74866572+olojo-99@users.noreply.github.com>
2022-09-18 12:19:15 +0100

    Update execute.c

--- src/execute.c
+++ src/execute.c
@@ -64,9 +64,3 @@ void execution(char **arg, char *args[], char *buf)
 
     }
 }
-
-/*
-I certify that this assignment is my own work carried out in a manner pursuant to the DCU Academic Integrity Policy,
-based on my personal study and/or research and that I have acknowledged all material and sources used in its preparation.
-Benjamin Olojo - 19500599
-*/
